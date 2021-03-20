0fecdd9bc17f62b2795e0d4f9c29a7870fdb25f6
CSN-001 <1354252799@qq.com>
2021-03-20 16:40:45 +0800

    success

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pyopenpose as op
 import cv2
+import matplotlib.pyplot as plt
 
 params = dict()
-params["model_folder"] = "./openpose/models/"
-params["face"] = True
-params["hand"] = True
+params["model_folder"] = "../openpose/models/"
 
 # Starting OpenPose
 opWrapper = op.WrapperPython()
@@ -13,8 +12,12 @@ opWrapper.start()
 
 datum = op.Datum()
 imageToProcess = cv2.imread("img.jpg")
+# cv2.imshow("qq", imageToProcess)
+imageToProcess = cv2.cvtColor(imageToProcess, cv2.COLOR_RGB2BGR)
+
 datum.cvInputData = imageToProcess
 opWrapper.emplaceAndPop(op.VectorDatum([datum]))
 
 print("Body keypoints: \n" + str(datum.poseKeypoints))
-cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", datum.cvOutputData)
+cv2.imwrite("res.jpg", datum.cvOutputData)
+
