07d470d8d2be58ea1224b51659530e41c524827e
CSN-001 <1354252799@qq.com>
2021-03-21 15:09:03 +0800

    save

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pyopenpose as op
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 
 params = dict()
 params["model_folder"] = "../openpose/models/"
@@ -12,12 +12,11 @@ opWrapper.start()
 
 datum = op.Datum()
 imageToProcess = cv2.imread("img.jpg")
-# cv2.imshow("qq", imageToProcess)
-# imageToProcess = cv2.cvtColor(imageToProcess, cv2.COLOR_RGB2BGR)
 
 datum.cvInputData = imageToProcess
 opWrapper.emplaceAndPop(op.VectorDatum([datum]))
 
 print("Body keypoints: \n" + str(datum.poseKeypoints))
-cv2.imwrite("res.jpg", datum.cvOutputData)
+np.savetxt("res.txt", ',')
+# cv2.imwrite("res.jpg", datum.cvOutputData)
 
